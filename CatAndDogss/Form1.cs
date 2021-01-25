806eb9460279b6ebd2ed7064e3ebd80ddac9b8e9
Opilane TTHK <opilane@tthk.ee>
2021-01-25 13:59:23 +0200

    mulle meeldivad mehed -erik

---
+++ CatAndDogss/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatAndDogss
+{
+    public partial class Form1 : Form
+    {
+        private object dogPicture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            string dogImage = GetDogImageUrl();
+
+            pictureBox1.ImageLocation = dogImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string GetDogImageUrl()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            string imageUrl;
+
+            using (var responsereader = new StreamReader(webStream))
+            {
+                var response = responsereader.ReadToEnd();
+                Dog dog = JsonConvert.DeserializeObject<Dog>(response);
+
+                imageUrl = dog.message;
+            }
+
+            return imageUrl;
+        }
+    }
+}
